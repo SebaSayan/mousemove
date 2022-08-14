38eeaf8a94566de998574de684c197c001678386
SebaSayan <sebastien-1985@live.fr>
2022-08-14 17:38:09 +0200

    Update mousemove

---
+++ index.js
@@ -0,0 +1,7 @@
+const mouses = document.querySelectorAll(".mouse");
+window.addEventListener("mousemove", (e) => {
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.pageY + "px";
+        mouse.style.left = e.pageX + "px";
+    });
+});
